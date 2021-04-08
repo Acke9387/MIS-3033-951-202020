af075a4542806901b75a33d769293bef60d24ed6
Ackerman, Adam P <adam@ou.edu>
2021-04-08 14:29:03 -0500

    Completed the MVC intro project

---
+++ 3013_Review/CreatingANewWindow/ExampleWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CreatingANewWindow
+{
+    /// <summary>
+    /// Interaction logic for ExampleWindow.xaml
+    /// </summary>
+    public partial class ExampleWindow : Window
+    {
+        public ExampleWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
