5f82fc9a3d1f233fad753d2bdf7ba2603a0c2217
Ackerman, Adam P <adamackrmn@gmail.com>
2021-04-29 13:37:59 -0500

    did an in class example over web api

---
+++ In_Class_Examples/WebAPI_Example/Controllers/StudentsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI_Example.Controllers
+{
+    public class StudentsController : ApiController
+    {
+
+        // GET api/Students
+
+        [Route("api/Students")]
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            List<Student> students = new List<Student>();
+            students.Add(new Student() { Classification = "Super Senior", Name = "Instructor Ackerman" });
+            students.Add(new Student() { Classification = "Senior", Name = "Talia Dean" });
+
+            return students;
+        }
+
+        [Route("api/Students/Best")]
+        [HttpGet]
+        public Student Puppy()
+        {
+            return new Student() { Classification = "Senior", Name = "Talia Dean" };
+
+        }
+
+
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public string Classification { get; set; }
+
+    }
+}
